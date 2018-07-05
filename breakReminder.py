7bc107e37517bf012c279cf80f5265b4f6f326f8
Shirish Surani <ssurani@mvixusa.com>
2018-07-05 15:36:57 -0400

    Inital Commit

---
+++ breakReminder.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+total_break = 3
+break_count = 0
+
+print "Program has started on " + time.ctime()
+
+while break_count < total_break:
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=scUSg51jWHI")
+    break_count = break_count+1
