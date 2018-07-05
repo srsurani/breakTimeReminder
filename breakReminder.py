0cc11a1bd5ce0c9bbcdb6345ad7a1d09b4f454a3
Shirish Surani <ssurani@mvixusa.com>
2018-07-05 15:41:44 -0400

    Changed interval time to 2 hours

--- breakReminder.py
+++ breakReminder.py
@@ -7,6 +7,6 @@ break_count = 0
 print "Program has started on " + time.ctime()
 
 while break_count < total_break:
-    time.sleep(10)
+    time.sleep(2*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=scUSg51jWHI")
     break_count = break_count+1
